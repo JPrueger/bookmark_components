fe7a45c1f5fd687ce33fb859b0def3e642d2c0aa
Jeannine Prueger <jeannine.prueger@gmail.com>
2020-10-05 15:50:01 +0200

    Added Component Register

---
+++ src/views/Register.vue
@@ -0,0 +1,29 @@
+<template> 
+
+    <div class="whole_content_wrapper">
+        <h1>Register here!</h1>
+
+        <form action="">
+            <div>
+                <label for="email">E-Mail Address (will be your username)</label> <br>
+                <input id="email" type="email" value="" name="email">
+            </div>
+
+            <div>
+                <label for="firstname">First Name:</label> <br>
+                <input id="firstname" type="text" value="" name="firstname">
+            </div>
+
+            <div>
+                <label for="lastname">Last Name:</label> <br>
+                <input id="lastname" type="text" value="" name="firstname">
+            </div>
+
+            <div>
+                <label for="password">Password:</label> <br>
+                <input id="password" type="password" value="" name="password">
+            </div>
+        </form>
+    </div>
+
+</template>
