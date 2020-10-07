acb6f27c31040502de39c18d732130819ea2e126
Jeannine Prueger <jeannine.prueger@gmail.com>
2020-10-07 15:23:07 +0200

    added view register (adapted p line height )

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import Home from "../views/Home.vue";
 import About from "../views/About.vue";
 import Post from "../views/Post.vue";
 import Mybookmarks from "../views/Mybookmarks.vue";
+import Register from "../views/Register.vue";
 
 const routes = [
   {
@@ -24,6 +25,11 @@ const routes = [
     path: "/my-bookmarks",
     name: "Mybookmarks",
     component: Mybookmarks,
+  },
+  {
+    path: "/register",
+    name: "Register",
+    component: Register,
   },
 ];
 
