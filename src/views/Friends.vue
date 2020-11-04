2ab8abc04e420e331d9d6d654262aacc1a4e8d68
Jeannine Prueger <jeannine.prueger@gmail.com>
2020-11-04 14:48:22 +0100

    Added view (Friends)

---
+++ src/views/Friends.vue
@@ -0,0 +1,39 @@
+<template>
+
+    <div class="home whole_content_wrapper">
+
+        <h1>Tom's profile!</h1>
+
+        <h3>What Tom has read last</h3>
+
+        <ProfileHasReadIt class="added_margin_bottom"></ProfileHasReadIt>
+
+        <h3>Friends</h3>
+
+        <div class="friends">
+            <ProfileFriendsOverview></ProfileFriendsOverview>
+        </div>
+
+    </div>
+
+</template>
+
+<script>
+    import ProfileHasReadIt from '@/components/ProfileHasReadIt.vue'
+    import ProfileFriendsOverview from '@/components/ProfileFriendsOverview.vue'
+
+
+    export default {
+        name: 'Friends',
+        components: {
+            ProfileHasReadIt,
+            ProfileFriendsOverview
+        }
+    }
+
+</script>
+
+<style lang="scss">
+    @import '@/assets/styles/variables.scss';
+
+</style>
